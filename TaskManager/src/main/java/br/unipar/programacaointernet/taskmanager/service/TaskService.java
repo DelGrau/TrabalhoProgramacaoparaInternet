c3452ef57d061a285632dad772dd7706420d5b0f
adrianlucast <adrianlucastoderke@hotmail.com>
2024-04-11 22:45:19 -0300

    Criado a função Gravar Historico e Mudanças no update

--- TaskManager/src/main/java/br/unipar/programacaointernet/taskmanager/service/TaskService.java
+++ TaskManager/src/main/java/br/unipar/programacaointernet/taskmanager/service/TaskService.java
@@ -25,22 +25,37 @@ public class TaskService {
         if (task == null)
             throw new Exception("Task = null");
         else {
-            Historico historico = new Historico();
-
-            historico.setDescricao(task.getDescricao());
-            historico.setData_alteracao(new Date());
-            historico.setObservacao(task.getObservacao());
-            historico.setPrioridade(task.getPrioridade().toString());
-            historico.setStatus(task.isStatus());
-            historico.setUsuario(task.getUsuario());
-            historico.setTask(task);
-
             taskRepository.cadastrar(task);
-            historicoRepository.cadastrar(historico);
+            gravarHistorico(task);
         }
     }
 
     public void deletar(Task task) throws Exception{
         taskRepository.deletar(task);
+    }
+
+    public void update(Task task) throws Exception{
+
+        Task oldTask = taskRepository.getTaskByID(task.getId());
+
+        //if (task.)
+        // Comparar os atributos do oldTask com os do Task
+        // Se o task for null, colocamos o oldTask no lugar
+        taskRepository.editar(task);
+        gravarHistorico(task);
+    }
+
+    private void gravarHistorico(Task task) throws Exception{
+        Historico historico = new Historico();
+
+        historico.setDescricao(task.getDescricao());
+        historico.setData_alteracao(new Date());
+        historico.setObservacao(task.getObservacao());
+        historico.setPrioridade(task.getPrioridade().toString());
+        historico.setStatus(task.isStatus());
+        historico.setUsuario(task.getUsuario());
+        historico.setTask(task);
+
+        historicoRepository.cadastrar(historico);
     }
 }
